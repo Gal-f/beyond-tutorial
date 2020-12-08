bd00d31d3ac0f7a53bce3f982cf19136fec986b1
Gal-f <galfroind@gmail.com>
2020-12-08 23:38:59 +0200

    Adding CI tests with flake8

--- test/test_examples.py
+++ test/test_examples.py
@@ -1,6 +1,9 @@
 """test_examples.py - tests for examples.py
 """
+import pytest
+
 from examples import power2
+from examples import fibo
 
 
 def test_power2():
@@ -8,6 +11,32 @@ def test_power2():
     assert power2(1) == 2
     assert power2(2) == 4
 
+"""
+@pytest.mark.parametrize('n,expected', [
+    (1,1),
+    (2,1),
+    (3,2),
+    (4,3),
+    (5,5),
+    (6,8)
+])
 
-def test_fibo():
-    assert False, "Please implement tests for fibo()"
+def test_fibo(n, expected):
+    assert fibo(n) == expected
+"""
+
+@pytest.mark.parametrize('n,expected', [
+    (1,1),
+    (2,1),
+    (7,13),
+    ...
+    (8,21),
+    (0, RuntimeError),
+    (-1, RuntimeError)
+])
+def test_fibo(n,expected):
+    if isinstance(expected,type) and issubclass(expected,Exception):
+        with pytest.raises(expected):
+            fibo(n)
+    else:
+        assert fibo(n) == expected
